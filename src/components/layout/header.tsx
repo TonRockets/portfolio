a0eaf5ced238f34deaf7a84df95e5615c8a1fc95
Wellington Pereira <wellington.pereira@a3data.com.br>
2025-01-23 20:59:56 -0300

    fix(layout): adjust header to correct render in mobile resolution

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -4,16 +4,16 @@ import { Container } from "components/ui/container"
 export const Header = () => {
   return (
     <header className="p-4">
-      <Container className="flex justify-between items-center">
+      <Container className="flex justify-between items-center xs:flex-col xs:gap-6">
         <div className="text-sm text-zinc-500">wellington.pereira@al.forsoft.org.br</div>
         <nav className="flex gap-4 text-sm text-zinc-500">
-          <Link href="https://linkedin.com/in/wellingtonpsantos" target="_blank" className="hover:text-white transition-colors">
+          <Link href="https://linkedin.com/in/wellingtonpsantos" target="_blank" className="hover:text-white transition-colors cursor-pointer">
             LinkedIn
           </Link>
-          <Link href="https://github.com/TonRockets" target="_blank" className="hover:text-white transition-colors">
+          <Link href="https://github.com/TonRockets" target="_blank" className="hover:text-white transition-colors cursor-pointer">
             GitHub
           </Link>
-          <Link href="https://instagram.com/ton.rockets" target="_blank" className="hover:text-white transition-colors">
+          <Link href="https://instagram.com/ton.rockets" target="_blank" className="hover:text-white transition-colors cursor-pointer">
             Instagram
           </Link>
         </nav>
